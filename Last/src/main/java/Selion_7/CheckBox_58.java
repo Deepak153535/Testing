f4ddf92fca48487fc1637be83fb4292cf93534bf
upsss <upsss@LAPTOP-SMGGKUE9>
2022-05-28 17:40:38 +0530

    Hello

--- Last/src/main/java/Selion_7/CheckBox_58.java
+++ Last/src/main/java/Selion_7/CheckBox_58.java
@@ -1,38 +1,25 @@
 package Selion_7;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class AutoSuggetion_56 {
+public class CheckBox_58 {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver.exe");
 		
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 		driver.manage().timeouts().implicitlyWait(300,TimeUnit.SECONDS);
 		
-		driver.findElement(By.id("autosuggest")).sendKeys("ind");
+		driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).click();
 		
-		List<WebElement>option=driver.findElements(By.xpath("//ul[@id='ui-id-1'] //a"));
 		Thread.sleep(3000);
-		for(WebElement options:option)
-		{
-			if(options.getText().equalsIgnoreCase("india"))
-			{
-				options.click();
-				break;
-			}
-		}
-		Thread.sleep(4000);
 		driver.quit();
-		
 	}
 }
